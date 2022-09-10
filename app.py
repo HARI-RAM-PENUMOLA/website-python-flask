3bf279ef6e8808a2746af5564430cf484c72d8b3
hariram <98948301+HARI-RAM-PENUMOLA@users.noreply.github.com>
2022-09-10 11:41:12 +0000

    work in progress

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Web developer',
+    'location': 'Hyderabad',
+}]
+
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Hash')
 
 
 if __name__ == "__main__":
